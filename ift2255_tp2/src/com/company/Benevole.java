397e52eb497bd415e077d9f9a6724a73fcfa412c
tanguybulliard <tanguybulliard@icloud.com>
2021-11-05 20:21:07 -0400

    Readme java
    j'ai ajouté le read me java, il est complet, à se référer pour compléter le code

---
+++ ift2255_tp2/src/com/company/Benevole.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Benevole extends Personne{
+    public Benevole(String id, String motDePasse, String nom, String prenom, String addresse, String codePostal, String ville, String Email, int cellulaire) {
+        super(id, motDePasse, nom, prenom, addresse, codePostal, ville, Email, cellulaire);
+    }
+}
